993f57512c90c8d3c26f49524452495b4e90c34a
Sauryadeep Roy <sauryadeep@alumnux.com>
2023-03-21 11:08:27 +0530

    Added login page

--- src/main/java/com/asl/login.java
+++ src/main/java/com/asl/login.java
@@ -3,7 +3,10 @@ package com.asl;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,24 +23,128 @@ public class login extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		 res.setContentType("text/html");
-//		PrintWriter out = res.getWriter();
-//		String email1=req.getParameter("email");
-//		out.print(email1);
-        Connection con=null;
-        try {
-        	con=Connector.getConnection();
-        	 
-        	
-        }catch(Exception e)
-        {
-        	e.printStackTrace();
-        }
-        System.out.println("success");
-      
-        
-		 
-	}
+	
+		protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+			
+			System.out.println("loginnnnnnnnnnnn!!!!!!!");
+			res.setContentType("text/html");
+			  PreparedStatement p = null;
+	          ResultSet rs = null;
+	          
+				PrintWriter out = res.getWriter();
+				
+				String email1=req.getParameter("email");
+				System.out.println(email1);
+		        Connection conn=null;
+		        int flag=0;
+		        try {
+		        	conn=Connector.getConnection();
+		        	System.out.println("Successs!!");
+		        	 String sql = "select * from EmployeesDetails";
+			            p = conn.prepareStatement(sql);
+			            rs = p.executeQuery();
+			           
+			 
+			           
+			            while (rs.next()) {
+			 
+			                String firstname1=rs.getString("FirstName");
+			                String middlename1=rs.getString("MiddleName");
+			                String lastname1=rs.getString("LastName");
+			                String email2 = rs.getString("email");
+			                String city1 = rs.getString("city");
+			                String dob1 = rs.getString("dob");
+			                String userid1=rs.getString("userid");
+			                
+			               
+			                if(email1.equals(email2))
+			                {
+			                	/*HttpSession ses=req.getSession();
+			                	ses.setAttribute("firstname", firstname1);
+			                	ses.setAttribute("lastname", lastname1);
+			                	ses.setAttribute("email", email2);
+			                	ses.setAttribute("city", city1);
+			                	ses.setAttribute("dob", dob1);
+			                	
+			                	res.sendRedirect("Profile");*/
+			                	
+			                	flag=1;
+			                	
+			                	 out.println("<html>");
+			                     out.println("<head><title>User Details</title></head>");
+			                     out.println("<body style='background-color: powderblue;'>");
+			                     out.println("<tr>");
+			                     out.print("<td><h1>FirstName:</td>");
+			                     out.print("<td>"+firstname1+"</h1></td>");
+			                     out.println("</tr>");
+			                    
+			                     if(middlename1!=null) {
+			                     out.println("<tr>");
+			                     out.print("<td><h1>MiddleName:</td>");
+			                     out.print("<td>"+middlename1+"</h1></td>");
+			                     out.println("</tr>");
+			                     }
+			                     
+			                     out.println("<tr>");
+			                     out.print("<td><h1>LastName:</td>");
+			                     out.print("<td>"+lastname1+"</h1></td>");
+			                     out.println("</tr>");
+			                     
+			                     
+			                     
+			                     
+			                     
+			                     out.println("<tr>");
+			                     out.println("<td><h1>email:</td>");
+			                     out.println("<td>"+email2+"</h1></td>");
+			                     out.println("</tr>");
+			                     
+			                     out.println("<tr>");
+			                     out.println("<td><h1>city:</td>");
+			                     out.println("<td>"+city1+"</h1></td>");
+			                     out.println("</tr>");
+			                     
+			                     
+			                     out.println("<tr>");
+			                     out.println("<td><h1>DOB:</td>");
+			                     out.println("<td>"+dob1+"</h1></td>");
+			                     out.println("</tr>");
+			                     
+			                     
+			                     out.println("<tr>");
+			                     out.println("<td><h1>UserId:</td>");
+			                     out.println("<td>"+userid1+"</h1></td>");
+			                     out.println("</tr>");
+			                     out.println("</body></html>");
+			                     
+			                     
+			                     
+			                     
+			                     
+			                     
+			                     
+			       
+
+			                }
+			            }
+		        	
+		        	 
+		        	
+		        }catch(Exception e)
+		        {
+		        	e.printStackTrace();
+		        }
+		        
+		        if(flag==0) {
+		        	RequestDispatcher rd=req.getRequestDispatcher("notpresent.html");
+		        	rd.include(req, res);
+		        	RequestDispatcher rd2=req.getRequestDispatcher("login.html");
+		        	rd2.include(req, res);
+		        	
+		        	
+		        }
+		        	
+		}
+
 
 }
