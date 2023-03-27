c7aa4c91c01e025778337ad14bb404a2125cde4d
chiragx09 <chirag.mundra@alumnux.com>
2023-03-27 14:32:19 +0530

    CONFIG FILE INCLUDED  AND CONNECTION GETTING CREATED

--- src/main/java/com/asl/connector/Connector.java
+++ src/main/java/com/asl/connector/Connector.java
@@ -1,6 +1,7 @@
 package com.asl.connector;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,7 +19,7 @@ public class Connector {
 		String password="";
 		String fileName = "src/main/java/com/asl/resources/config.properties";
 		//try {
-	//	Properties p=new Properties();
+		Properties p=new Properties();
 		
 //		ClassLoader classLoader = getClass().getClassLoader();
 	//	InputStream in=new FileInputStream(fileName);
@@ -37,11 +38,23 @@ public class Connector {
 	//	e.printStackTrace();
 		
 	 //   }
+		
+		TestClass ob= new TestClass();
+		try {
+			p.load(ob.getFileFromResourceAsStream("com/asl/resources/config.properties"));
+			url=(String)p.getProperty("url");
+			username=(String)p.getProperty("username");
+			password=(String)p.getProperty("password");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 			
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");  
-//			 con=DriverManager.getConnection(url,username,password);
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/asl", "asl", "fruit1");
+			 con=DriverManager.getConnection(url,username,password);
+			 System.out.println("connection created");
+//			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/asl", "asl", "fruit1");
 			
 			
 			
