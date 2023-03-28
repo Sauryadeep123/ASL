32e437b7426cfd54d32c2dae824e1de740de9c02
chiragx09 <chirag.mundra@alumnux.com>
2023-03-28 14:38:48 +0530

    edited register.jaaaaaaava

--- src/main/java/com/asl/loginregistration/Register.java
+++ src/main/java/com/asl/loginregistration/Register.java
@@ -10,6 +10,8 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,6 +23,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 import com.asl.connector.Connector;
+import com.asl.dbentity.EmployeeDetails;
+import com.asl.dboperations.InsertQuery;
+import com.asl.dboperations.SelectQuery;
+import com.asl.filehandling.FileHandling;
 
 /**
  * Servlet implementation class Register
@@ -35,117 +41,54 @@ public class Register extends HttpServlet {
 	 *      response)
 	 */
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		String firstName = req.getParameter("firstName");
-		String middleName = req.getParameter("middleName");
-		String lastName = req.getParameter("lastName");
-		String email = req.getParameter("email");
-		String password = req.getParameter("password");
-		String city = req.getParameter("city");
-		String dob = (String) req.getParameter("dob");
-		Part partPhoto = req.getPart("photo");
-		Part partResume = req.getPart("resume");
-		PrintWriter out = res.getWriter();
-		boolean flag = true;
-
+		EmployeeDetails emp=new EmployeeDetails();
+		
+		emp.setFirstName(req.getParameter("firstName"));
+		emp.setMiddleName(req.getParameter("middleName"));
+		emp.setLastName(req.getParameter("lastName"));
+		emp.setEmail(req.getParameter("email"));
+		emp.setPassword(req.getParameter("password"));
+		emp.setCity(req.getParameter("city"));
+		
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd-MM-yyyy");
+		java.util.Date date=null;
 		try {
-			Connection con = Connector.getConnection();
-
-			String sql = "select email from employees_details";
-			PreparedStatement p = con.prepareStatement(sql);
-			ResultSet rs = p.executeQuery();
-
-			while (rs.next()) {
-				String email2 = rs.getString("email");
-				if (email.equals(email2)) {
-					flag = false;
-					RequestDispatcher rd = req.getRequestDispatcher("AlreadyUser.html");
-					rd.include(req, res);
-					RequestDispatcher rd2 = req.getRequestDispatcher("Login.html");
-					rd2.include(req, res);
-				}
+			date = sdf1.parse(req.getParameter("dob"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		java.sql.Date sqlStartDate = new java.sql.Date(date.getTime());
+		emp.setDob(sqlStartDate);
+		emp.setPassword(req.getParameter("password"));
+		
+		if(new SelectQuery().alreadyUser(emp.getEmail())) {
+			
+		}
+		else {
+			Part partPhoto = req.getPart("photo");
+			Part partResume = req.getPart("resume");
+			String path = "/home/chirag/eclipse-workspace/ASL/";
+			try {
+				emp.setPhoto(new FileHandling().saveFile(path + "photos", partPhoto,emp.getEmail()));
+				emp.setResume(new FileHandling().saveFile(path + "resume", partResume,emp.getEmail()));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-			if (flag) {
-				PreparedStatement ps = con.prepareStatement(
-						"INSERT INTO employees_details (FirstName,MiddleName,LastName,email,city,dob,userid,photo,resume)"
-								+ " values (?,?,?,?,?,?,?,?,?);");
-				ps.setString(1, firstName);
-				ps.setString(2, middleName);
-				ps.setString(3, lastName);
-				ps.setString(4, email);
-				ps.setString(5, city);
-				ps.setString(6, dob);
-				ps.setString(7, "not set yet");
-				ps.setString(8,getFileName(partPhoto,email));
-				ps.setString(9,getFileName(partResume,email));
-
-			int i=ps.executeUpdate();
 			
-			ps = con.prepareStatement(
-					"INSERT INTO employees_list (FirstName,email,userid)"
-							+ " values (?,?,?);");
-			ps.setString(1, firstName);
-			ps.setString(2, email);
-			ps.setString(3, "ALU9898");
-			ps.executeUpdate();
+			new InsertQuery().insertIntoEmployees(emp);
+			new InsertQuery().insertIntoEmployeeDetails(emp,new SelectQuery().getId(emp.getEmail()));
 			
-			ps = con.prepareStatement(
-					"INSERT INTO login_details (email,password)"
-							+ " values (?,?);");
-			ps.setString(1, email);
-			ps.setString(2, password);
-			ps.executeUpdate();
-			
-//			
-			if(i==1)
-				{
-//				
-					String path = "/home/chirag/eclipse-workspace/ASL/";
-
-					saveFile(path + "photos", partPhoto,email);
-					saveFile(path + "resume", partResume,email);
-
+		}
+		
+		PrintWriter out = res.getWriter();
+		
 					RequestDispatcher rd=req.getRequestDispatcher("RegistrationSuccess.html");
 		        	rd.include(req, res);
 		        	RequestDispatcher rd2=req.getRequestDispatcher("Login.html");
 		        	rd2.include(req, res);
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private String getFileName(final Part part,String email) {
-		final String partHeader = part.getHeader("content-disposition");
-		for (String content : partHeader.split(";")) {
-			if (content.trim().startsWith("filename")) {
-				return email+content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
-			}
-		}
-		return null;
-	}
-
-	private String saveFile(String uploadPath, Part part,String email) throws Exception {
-		File uploadDir = new File(uploadPath);
-		if (!uploadDir.exists()) {
-			uploadDir.mkdir();
-		}
-
-		String fileName = getFileName(part,email);
-		OutputStream out1 = new FileOutputStream(new File(uploadPath + File.separator + fileName));
-		InputStream fileContent = part.getInputStream();
-		int read = 0;
-		final byte[] bytes = new byte[1024];
-		while ((read = fileContent.read(bytes)) != -1) {
-			out1.write(bytes, 0, read);
-		}
-		out1.flush();
-		out1.close();
-		fileContent.close();
-
-		return "";
+				
 	}
 
 }
