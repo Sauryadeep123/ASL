4c2bfd1a67e2beb8afbfe0b69d0e4e2f2d846484
Sauryadeep Roy <sauryadeep@alumnux.com>
2023-04-03 16:20:21 +0530

    apply toolpit

--- src/main/webapp/javascript/register.js
+++ src/main/webapp/javascript/register.js
@@ -137,7 +137,7 @@ function buildTable(data) {
 								var row = `<tr>
 							<td>${data[i].serialNo}</td>
 							<td><span class="truncate">${data[i].firstName}<span class="tooltiptext">${data[i].firstName}</span></span></td>
-							<td>${data[i].email}</td>
+						<td><span class="truncate">${data[i].email}<span class="tooltiptext">${data[i].email}</span></span></td>
 							<td><img src="photo/${data[i].photo}" alt="" style="width:50px;height:50px"></td>
 							<td></td>
 						
